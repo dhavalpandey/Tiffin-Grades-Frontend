43cb1aa9b8b6784557e1827b434d03745d36f415
Dhaval Pandey <dhavalpandey2007@outlook.com>
2022-03-05 11:48:48 +0000

    migrationtoheroku

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = global.config = {
   development: {
     status: false,
   },
-  url: "https://tiffingrades-api.azurewebsites.net",
+  url: "https://tiffingrades-api.herokuapp.com",
   predictURI: "http://127.0.0.1:5000",
   predictURL: "https://tiffingrades-predict.azurewebsites.net",
 };
